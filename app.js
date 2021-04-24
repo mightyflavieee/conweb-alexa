ea151513b405451493cdb219bad76de392bb57ce
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-04-24 18:17:02 +0200

    fixing route

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const apiRoutes = require('./routes/index');
 const app = express();
 
-app.use('/api', apiRoutes);
+app.use('/', apiRoutes);
 app.listen(5000);
 console.log('Listening at 5000');
