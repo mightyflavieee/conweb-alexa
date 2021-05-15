e5480f025152ec33211f84110105417ff3079708
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-05-15 23:33:27 +0200

    added socker connection to python framework
    Co-authored-by: Leonardo Mussato <LeonardoMussato@users.noreply.github.com>

--- controllers/alexa.js
+++ controllers/alexa.js
@@ -1,75 +1,74 @@
-const Alexa = require('ask-sdk');
-const https = require('https');
-const ws = require('ws');
+const Alexa = require("ask-sdk");
+const https = require("https");
+const ws = require("ws");
+const botConnector = require("./botConnector");
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    return handlerInput.requestEnvelope.request.type === "LaunchRequest";
   },
   handle(handlerInput) {
+    const connector = BotConnector.getInstance();
+    const idConnection = handlerInput.requestEnvelope.session.user.userId;
+    connector.addConnection(idConnection);
     return handlerInput.responseBuilder
-      .speak('Hi. How can I help you?')
-      .reprompt('I didn\'t catch that. What can I help you with?')
+      .speak("Hi. How can I help you?")
+      .reprompt("I didn't catch that. What can I help you with?")
       .getResponse();
   },
 };
 
 const StopRequestHandler = {
   canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request
-    return request.type === 'SessionEndedRequest' ||
-      (request.type === 'IntentRequest' &&
-        request.intent.name === 'AMAZON.StopIntent');
+    const request = handlerInput.requestEnvelope.request;
+    return (
+      request.type === "SessionEndedRequest" ||
+      (request.type === "IntentRequest" &&
+        request.intent.name === "AMAZON.StopIntent")
+    );
   },
   handle(handlerInput) {
     return handlerInput.responseBuilder
-      .speak('Ok master. GoodBye.')
+      .speak("Ok master. GoodBye.")
       .getResponse();
   },
 };
 
-
 const SendMessageRequestHandler = {
   canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request
-    return request.type === 'IntentRequest' &&
-      request.intent.name === 'SendMessage';
+    const request = handlerInput.requestEnvelope.request;
+    return (
+      request.type === "IntentRequest" && request.intent.name === "SendMessage"
+    );
   },
   async handle(handlerInput) {
-    const message = handlerInput.requestEnvelope.request.intent.slots.message.value;
+    const message =
+      handlerInput.requestEnvelope.request.intent.slots.message.value;
 
-    /*let returnMessage = '';
-    await new Promise((resolve, reject) => {
-      https.get('https://www.google.com', function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function() {});
-        res.on('end', function(body) {
-          console.log(message);
-          returnMessage = message;
-          resolve();
-        });
-      });
-    });*/
+    const idConnection = handlerInput.requestEnvelope.session.user.userId;
+    const connector = BotConnector.getInstance();
+    const connection = connector.getConnection(idConnection);
 
-    let socket = new WebSocket("wss://localhost/alexa:8080");
-    console.log("Connection established with Python Bot");
-    socket.send(message);
-    socket.onmessage = (event) => {
-      socket.close();
-      return handlerInput.responseBuilder.speak(event.data).reprompt().getResponse();;
-    }
+    connection.send(message);
 
-    // return handlerInput.responseBuilder.speak('Called api: ' + returnMessage).reprompt().getResponse();
+    const response = await new Promise((resolve, reject) => {
+      connection.on("message", (response) => resolve(response));
+    });
+
+    return handlerInput.responseBuilder
+      .speak(response)
+      .reprompt()
+      .getResponse();
   },
 };
 
 const skillBuilder = Alexa.SkillBuilders.custom()
-.withSkillId('amzn1.ask.skill.70b73154-805a-4ef4-bd91-3ccb95f548ce')
-.addRequestHandlers(
-  LaunchRequestHandler,
-  StopRequestHandler,
-  SendMessageRequestHandler
-);
+  .withSkillId("amzn1.ask.skill.70b73154-805a-4ef4-bd91-3ccb95f548ce")
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    StopRequestHandler,
+    SendMessageRequestHandler
+  );
 
 const skill = skillBuilder.create();
 module.exports = skill;
---
+++ controllers/botConnector.js
@@ -0,0 +1,50 @@
+///
+/*.exports = {
+    const connections : [],
+
+    function create/*Connection(id){
+        let socket = new WebSocket("wss://localhost/alexa:8080");
+        console.log("Connection established with Python Bot");
+        socket.onmessage = (event) => {};
+        connections.push({
+            id: id
+        });
+    }
+};
+
+*/
+
+class PrivateBotConnector {
+  constructor() {
+    console.log("Bot connector created.");
+  }
+
+  connections = [];
+
+  addConnection(id) {
+    const socketConnection = new WebSocket("wss://localhost/browse:5050")
+      .onclose((event) => console.log(event))
+      .onopen((socket) => console.log(socket));
+    this.connections.push({
+      id: socketConnection,
+    });
+  }
+
+  getConnection(id) {
+    return this.connections.find((map) => map.id === id);
+  }
+}
+
+class BotConnector {
+  constructor() {
+    throw new Error("Cannot create instance");
+  }
+  static getInstance() {
+    if (!BotConnector.instance) {
+      BotConnector.instance = new PrivateBotConnector();
+    }
+    return BotConnector.instance;
+  }
+}
+
+module.exports = BotConnector;
