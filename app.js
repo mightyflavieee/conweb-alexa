7e176ebc0b786ec6118199232d9620f7a7582e44
LeonardoMussato <leo.muss@gmail.com>
2021-05-24 14:12:04 +0100

    removed web socket

--- app.js
+++ app.js
@@ -5,16 +5,3 @@ const app = express();
 app.use('/', apiRoutes);
 app.listen(5000);
 console.log('Listening at 5000');
-
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 6666 });
-
-console.log("Server up!");
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    ws.send('amazing beautiful response!');
-
-  });
-});
